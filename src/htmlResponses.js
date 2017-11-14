6a314d508b380b53680dadc21ce2587881a28d19
Ian Cavanaugh <flameian@mac.com>
2017-11-14 08:49:23 -0500

    done

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -1,4 +1,4 @@
-const fs = require('fs'); // pull in the file system module
+const fs = require("fs"); // pull in the file system module
 
 const index = fs.readFileSync(`${__dirname}/../hosted/404.html`);
 const css = fs.readFileSync(`${__dirname}/../hosted/style.css`);
@@ -24,154 +24,154 @@ const p5 = fs.readFileSync(`${__dirname}/../hosted/P/P5.html`);
 
 
 const getIndex = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(index);
-  response.end();
+	response.writeHead(200, { "Content-Type": "text/html" });
+	response.write(index);
+	response.end();
 };
 
 const getA1 = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(a1);
-  response.end();
+	response.writeHead(200, { "Content-Type": "text/html" });
+	response.write(a1);
+	response.end();
 };
 
 const getA2 = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(a2);
-  response.end();
+	response.writeHead(200, { "Content-Type": "text/html" });
+	response.write(a2);
+	response.end();
 };
 
 const getA3 = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(a3);
-  response.end();
+	response.writeHead(200, { "Content-Type": "text/html" });
+	response.write(a3);
+	response.end();
 };
 
 const getA4 = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(a4);
-  response.end();
+	response.writeHead(200, { "Content-Type": "text/html" });
+	response.write(a4);
+	response.end();
 };
 
 const getA5 = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(a5);
-  response.end();
+	response.writeHead(200, { "Content-Type": "text/html" });
+	response.write(a5);
+	response.end();
 };
 
 const getA6 = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(a6);
-  response.end();
+	response.writeHead(200, { "Content-Type": "text/html" });
+	response.write(a6);
+	response.end();
 };
 
 const getA7 = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(a7);
-  response.end();
+	response.writeHead(200, { "Content-Type": "text/html" });
+	response.write(a7);
+	response.end();
 };
 
 const getA8 = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(a8);
-  response.end();
+	response.writeHead(200, { "Content-Type": "text/html" });
+	response.write(a8);
+	response.end();
 };
 
 const getD1 = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(d1);
-  response.end();
+	response.writeHead(200, { "Content-Type": "text/html" });
+	response.write(d1);
+	response.end();
 };
 
 const getD2 = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(d2);
-  response.end();
+	response.writeHead(200, { "Content-Type": "text/html" });
+	response.write(d2);
+	response.end();
 };
 
 const getD3 = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(d3);
-  response.end();
+	response.writeHead(200, { "Content-Type": "text/html" });
+	response.write(d3);
+	response.end();
 };
 
 const getH1 = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(h1);
-  response.end();
+	response.writeHead(200, { "Content-Type": "text/html" });
+	response.write(h1);
+	response.end();
 };
 
 const getH2 = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(h2);
-  response.end();
+	response.writeHead(200, { "Content-Type": "text/html" });
+	response.write(h2);
+	response.end();
 };
 
 const getP1 = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(p1);
-  response.end();
+	response.writeHead(200, { "Content-Type": "text/html" });
+	response.write(p1);
+	response.end();
 };
 
 const getP2 = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(p2);
-  response.end();
+	response.writeHead(200, { "Content-Type": "text/html" });
+	response.write(p2);
+	response.end();
 };
 
 const getP3 = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(p3);
-  response.end();
+	response.writeHead(200, { "Content-Type": "text/html" });
+	response.write(p3);
+	response.end();
 };
 
 const getP4 = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(p4);
-  response.end();
+	response.writeHead(200, { "Content-Type": "text/html" });
+	response.write(p4);
+	response.end();
 };
 
 const getP5 = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(p5);
-  response.end();
+	response.writeHead(200, { "Content-Type": "text/html" });
+	response.write(p5);
+	response.end();
 };
 
 // return the CSS file
 const getCSS = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/css' });
-  response.write(css);
-  response.end();
+	response.writeHead(200, { "Content-Type": "text/css" });
+	response.write(css);
+	response.end();
 };
 
 // return our bundled js
 const getBundle = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/javascript' });
-  response.write(bundle);
-  response.end();
+	response.writeHead(200, { "Content-Type": "text/javascript" });
+	response.write(bundle);
+	response.end();
 };
 
 
 module.exports = {
-  getIndex,
-  getCSS,
-  getBundle,
-  getA1,
-  getA2,
-  getA3,
-  getA4,
-  getA5,
-  getA6,
-  getA7,
-  getA8,
-  getD1,
-  getD2,
-  getD3,
-  getH1,
-  getH2,
-  getP1,
-  getP2,
-  getP3,
-  getP4,
-  getP5,
+	getIndex,
+	getCSS,
+	getBundle,
+	getA1,
+	getA2,
+	getA3,
+	getA4,
+	getA5,
+	getA6,
+	getA7,
+	getA8,
+	getD1,
+	getD2,
+	getD3,
+	getH1,
+	getH2,
+	getP1,
+	getP2,
+	getP3,
+	getP4,
+	getP5,
 };
--- src/server.js
+++ src/server.js
@@ -1,66 +1,65 @@
-const http = require('http');
-const url = require('url');
-const query = require('querystring');
-const htmlHandler = require('./htmlResponses.js');
+const http = require("http");
+const url = require("url");
+const query = require("querystring");
+const htmlHandler = require("./htmlResponses.js");
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 // deal with GET requests
 const handleGet = (request, response, parsedUrl) => {
-  console.log(parsedUrl.pathname);
-  const res = response;
+	console.log(parsedUrl.pathname);
 
-  if (parsedUrl.pathname.includes('/style.css')) {
-    htmlHandler.getCSS(request, response);
-  } else if (parsedUrl.pathname.includes('/es5Scripts.js')) {
-    htmlHandler.getBundle(request, response);  
-  } else if (parsedUrl.pathname === '/A/A1.html') {
-    htmlHandler.getA1(request, response);
-  } else if (parsedUrl.pathname === '/A/A2.html') {
-    htmlHandler.getA2(request, response);
-  } else if (parsedUrl.pathname === '/A/A3.html') {
-    htmlHandler.getA3(request, response);
-  } else if (parsedUrl.pathname === '/A/A4.html') {
-    htmlHandler.getA4(request, response);
-  } else if (parsedUrl.pathname === '/A/A5.html') {
-    htmlHandler.getA5(request, response);
-  } else if (parsedUrl.pathname === '/A/A6.html') {
-    htmlHandler.getA6(request, response);
-  } else if (parsedUrl.pathname === '/A/A7.html') {
-    htmlHandler.getA7(request, response);
-  } else if (parsedUrl.pathname === '/A/A8.html') {
-    htmlHandler.getA8(request, response);
-  } else if (parsedUrl.pathname === '/D/D1.html') {
-    htmlHandler.getD1(request, response);
-  } else if (parsedUrl.pathname === '/D/D2.html') {
-    htmlHandler.getD2(request, response);
-  } else if (parsedUrl.pathname === '/D/D3.html') {
-    htmlHandler.getD3(request, response);
-  } else if (parsedUrl.pathname === '/H/H1.html') {
-    htmlHandler.getH1(request, response);
-  } else if (parsedUrl.pathname === '/H/H2.html') {
-    htmlHandler.getH2(request, response);
-  } else if (parsedUrl.pathname === '/P/P1.html') {
-    htmlHandler.getP1(request, response);
-  } else if (parsedUrl.pathname === '/P/P2.html') {
-    htmlHandler.getP2(request, response);
-  } else if (parsedUrl.pathname === '/P/P3.html') {
-    htmlHandler.getP3(request, response);
-  } else if (parsedUrl.pathname === '/P/P4.html') {
-    htmlHandler.getP4(request, response);
-  } else if (parsedUrl.pathname === '/P/P5.html') {
-    htmlHandler.getP5(request, response);
-  } else if (parsedUrl.pathname.includes('/favicon.ico')) {
-  } else {
-    htmlHandler.getIndex(request, response);
-  }
+	if (parsedUrl.pathname.includes("/style.css")) {
+		htmlHandler.getCSS(request, response);
+	} else if (parsedUrl.pathname.includes("/es5Scripts.js")) {
+		htmlHandler.getBundle(request, response);  
+	} else if (parsedUrl.pathname === "/A/A1.html") {
+		htmlHandler.getA1(request, response);
+	} else if (parsedUrl.pathname === "/A/A2.html") {
+		htmlHandler.getA2(request, response);
+	} else if (parsedUrl.pathname === "/A/A3.html") {
+		htmlHandler.getA3(request, response);
+	} else if (parsedUrl.pathname === "/A/A4.html") {
+		htmlHandler.getA4(request, response);
+	} else if (parsedUrl.pathname === "/A/A5.html") {
+		htmlHandler.getA5(request, response);
+	} else if (parsedUrl.pathname === "/A/A6.html") {
+		htmlHandler.getA6(request, response);
+	} else if (parsedUrl.pathname === "/A/A7.html") {
+		htmlHandler.getA7(request, response);
+	} else if (parsedUrl.pathname === "/A/A8.html") {
+		htmlHandler.getA8(request, response);
+	} else if (parsedUrl.pathname === "/D/D1.html") {
+		htmlHandler.getD1(request, response);
+	} else if (parsedUrl.pathname === "/D/D2.html") {
+		htmlHandler.getD2(request, response);
+	} else if (parsedUrl.pathname === "/D/D3.html") {
+		htmlHandler.getD3(request, response);
+	} else if (parsedUrl.pathname === "/H/H1.html") {
+		htmlHandler.getH1(request, response);
+	} else if (parsedUrl.pathname === "/H/H2.html") {
+		htmlHandler.getH2(request, response);
+	} else if (parsedUrl.pathname === "/P/P1.html") {
+		htmlHandler.getP1(request, response);
+	} else if (parsedUrl.pathname === "/P/P2.html") {
+		htmlHandler.getP2(request, response);
+	} else if (parsedUrl.pathname === "/P/P3.html") {
+		htmlHandler.getP3(request, response);
+	} else if (parsedUrl.pathname === "/P/P4.html") {
+		htmlHandler.getP4(request, response);
+	} else if (parsedUrl.pathname === "/P/P5.html") {
+		htmlHandler.getP5(request, response);
+	} else if (parsedUrl.pathname.includes("/favicon.ico")) {
+	} else {
+		htmlHandler.getIndex(request, response);
+	}
 };
 
 // figure out what kind of request we're dealing with
 const onRequest = (request, response) => {
-  const parsedUrl = url.parse(request.url);
+	const parsedUrl = url.parse(request.url);
 
-  handleGet(request, response, parsedUrl);
+	handleGet(request, response, parsedUrl);
 };
 
 http.createServer(onRequest).listen(port);
